963ac971ca406de2cd2c637b8a0e34ca17832a14
e.aleixandre <e.aleixandre@ourown.studio>
2026-09-04 13:31:52 +0000

    chore(mobile): keep rail change scoped

--- pkg/serve/static/build/e356a8dceee2/app.js
+++ pkg/serve/static/build/e356a8dceee2/app.js
@@ -218,4 +218,4 @@ dompurify/dist/purify.es.mjs:
   (*! @license DOMPurify 3.4.12 | (c) Cure53 and other contributors | Released under the Apache license 2.0 and Mozilla Public License 2.0 | github.com/cure53/DOMPurify/blob/3.4.12/LICENSE *)
 */
 
-globalThis.__MOA_BUILD_ID__="3284bae79fd7";
+globalThis.__MOA_BUILD_ID__="e356a8dceee2";
