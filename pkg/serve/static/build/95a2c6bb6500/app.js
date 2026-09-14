bad3c82ce12c4cec9fb1c2b74135c8434b7acaa1
e.aleixandre <e.aleixandre@ourown.studio>
2026-09-14 22:42:32 +0000

    fix(frontend): retain the artifact entry control rule

--- pkg/serve/static/build/95a2c6bb6500/app.js
+++ pkg/serve/static/build/95a2c6bb6500/app.js
@@ -206,4 +206,4 @@ dompurify/dist/purify.es.mjs:
   (*! @license DOMPurify 3.4.12 | (c) Cure53 and other contributors | Released under the Apache license 2.0 and Mozilla Public License 2.0 | github.com/cure53/DOMPurify/blob/3.4.12/LICENSE *)
 */
 
-globalThis.__MOA_BUILD_ID__="c53848ff8be9";
+globalThis.__MOA_BUILD_ID__="95a2c6bb6500";
